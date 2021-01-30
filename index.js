30d77ff5cdb9e823c56fdb9d22b78e03a6476acc
david <david@david.com>
2021-01-29 16:50:45 -0800

    working file query, should be largely done

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ const app = express();
 app.use('/static', express.static(path.join(__dirname, 'pictures')));
 console.log(path.join(__dirname, 'pictures'));
 
-// An api endpoint that returns a short list of items
-app.get('/avail', (req,res) => {
-    console.log('Sent list of items');
+function parse_list(ext){
+  var list = "{ ";
+  var name = new RegExp("name[a-zA-Z]*");
+  var birth = new RegExp("birth[a-zA-Z]*");
+  var description = new RegExp("desc[a-zA-Z]*");
+  var color = new RegExp("color[a-zA-Z]*");
+  const fs = require('fs');
 
-    var dir = path.join(__dirname+'/available');
-    var list = '{ "available" : [';
-    var name = new RegExp("name[a-zA-Z]*");
-    var birth = new RegExp("birth[a-zA-Z]*");
-    var description = new RegExp("desc[a-zA-Z]*");
-    var color = new RegExp("color[a-zA-Z]*");
+  var count = 0;
+  var count_dirs = 0;
+  var dirs = [ext];
 
-    const fs = require('fs');
+  if (ext == "available"){
+    dirs.push("expected");
+    dirs.push("past");
+  }
 
-
+  dirs.forEach((dir_short) => {
+    var dir = path.join(__dirname +'/' + dir_short);
+    list += '"' + dir_short + '" : [';
     var filenames = fs.readdirSync(dir, 'utf8');
-    var count = 0;
 
     filenames.forEach(function(filename){
       var data = fs.readFileSync(path.join(dir,filename), 'utf8');
@@ -56,22 +61,76 @@ app.get('/avail', (req,res) => {
         }
         if (count2 < (lines.length - 2)){
           count2++;
-          list += ","
+          list += ",";
         }
       });
-      list += '}'
+      list += '}';
 
-      if (count == (filenames.length - 1)){
-        list = list + ']}';
-      }
-      else {
+      if (count != (filenames.length - 1)){
         count++;
         list = list + ', ';
       }
 
     })
 
-    var val = JSON.parse(list);
+    if (count_dirs < (dirs.length-1)){
+      count_dirs++;
+      list += '],';
+    }
+    else {
+      count_dirs++;
+      list += ']}';
+    }
+  })
+  return list;
+}
+
+// An api endpoint that returns a short list of items
+app.get('/queen', (req,res) => {
+
+    var queen = parse_list("queen");
+    var val = JSON.parse(queen);
+    console.log(val);
+
+    res.json(val);
+});
+
+// An api endpoint that returns a short list of items
+app.get('/stud', (req,res) => {
+
+    var stud = parse_list("stud");
+    var val = JSON.parse(stud);
+    console.log(val);
+
+    res.json(val);
+});
+
+// An api endpoint that returns a short list of items
+app.get('/past', (req,res) => {
+
+    var past = parse_list("past");
+    var val = JSON.parse(past);
+    console.log(val);
+
+    res.json(val);
+});
+
+// An api endpoint that returns a short list of items
+app.get('/expected', (req,res) => {
+
+    var expected = parse_list("expected");
+    var val = JSON.parse(expected);
+    console.log(val);
+
+    res.json(val);
+});
+
+// An api endpoint that returns a short list of items
+app.get('/avail', (req,res) => {
+
+    var avail = parse_list("available");
+    console.log(avail);
+    var val = JSON.parse(avail);
     console.log(val);
 
     res.json(val);
