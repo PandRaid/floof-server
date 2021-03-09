cff0786a118507a87b14ecb96db8189726ab1ec8
david <david@david.com>
2021-03-08 22:14:32 -0800

    Working emailer, emailjs

--- index.js
+++ index.js
@@ -181,49 +181,6 @@ app.get('/avail', (req,res) => {
     res.json(val);
 });
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", //replace with your email provider
-  port: 587,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
-});
-
-transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
-
-app.post('/send', (req, res, next) => {
-  var name = req.body.name
-  var email = req.body.email
-  var message = req.body.message
-
-  var mail = {
-    from: name,
-    to: // receiver email,
-    subject: "dogs",
-    text: message
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
-  })
-})
-
-
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
